b1a5b79687a49741c89893f852db08e2c49473cd
Dhanraj-Naik <naik.dhanraj01@gmail.com>
2022-04-30 13:04:31 +0530

    splash screen api added and implemented

--- app/src/main/java/com/dj/universaldownloader/MainActivity.kt
+++ app/src/main/java/com/dj/universaldownloader/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.dj.universaldownloader
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
+
+    var keepSplashOnScreen = true
+    val delay = 2000L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //handle the splash screen transition
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition {
+            keepSplashOnScreen
+            //viewModel.isInitializationComplete()
+        }
+        Handler(Looper.getMainLooper()).postDelayed({ keepSplashOnScreen = false }, delay)
         setContentView(R.layout.activity_main)
     }
+
 }
